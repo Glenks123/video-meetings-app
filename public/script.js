a06d68a1f1a5e7b4dccb5c36934161cf32e947a1
Glenks123 <63196484+Glenks123@users.noreply.github.com>
2021-05-03 17:30:45 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 const socket = io('/');
-// we are passing in undefined because we will let the server generate it's own ID for each new connection
 const myPeer = new Peer(undefined, {
     host: '/',
     port: 3001
@@ -22,14 +21,12 @@ const peers = {}
 var getUserMedia = navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia;
 
 getUserMedia({
-    // we want to get out audio and video to send to other people
     video: true,
     audio: true,     
 }).then(stream => {
     //console.log(stream)
     addVideoStream(myVideo, stream);
 
-    // listening to when someone tries to call us and send them out current stream
     myPeer.on('call', call => {
         call.answer(stream);
         const video = document.createElement('video');
@@ -54,11 +51,7 @@ socket.on('user-disconnected', (userID, username) => {
     }
 })
 
-//on open means that as soon as we connect to out peer server and get back an ID, we want to run this code 
 myPeer.on('open', id => {
-    // ROOM_ID is defined in the ejs file
-    // giving back ROOM_ID to be accessed in the server
-    // id is the user id that myPeer generates whenever a new user logs in
     socket.emit('join-room', ROOM_ID, id, USERNAME);
 });
 
@@ -67,7 +60,6 @@ function connectToNewUser(userID, stream){
     // calls the user that we give the certain ID to and sending them out audio and stream
     const call = myPeer.call(userID, stream);
     const video = document.createElement('video');
-    // taking the stream from the other user and adding it to out videoGrid
     call.on('stream', userVideoStream => {
         //console.log(userVideoStream);
         addVideoStream(video, userVideoStream);
@@ -79,9 +71,7 @@ function connectToNewUser(userID, stream){
 }
 
 function addVideoStream(video, stream){
-    // this wil allows us to play our video
     video.srcObject = stream;
-    // this basically means, when the video is loaded on our page, we want it to play
     video.addEventListener('loadedmetadata', () => {
         video.play();
     });
@@ -112,7 +102,6 @@ socket.on('participants-list', users => {
 */
 
 
-// when the form is submitted, we receive the message and send it back to the server
 chatForm.addEventListener('submit', event => {
 
     event.preventDefault();
